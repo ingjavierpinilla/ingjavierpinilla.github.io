9d98e7218b74f91b6b30ab3dffa0c0e4b258082c
Javier <ing.jmpinillag@gmail.com>
2020-12-12 16:30:36 +0100

    Tpy fixed tittle->title

--- src/githubSite/products/models.py
+++ src/githubSite/products/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    tittle = models.TextField()
+    """modelo de producto
+
+    Args:
+        models ([type]): [description]
+
+    Returns:
+        [str]: [returns title ]
+    """
+    title = models.TextField()
     description = models.TextField()
 
     def __str__(self):
-        return self.tittle
+        return self.title
