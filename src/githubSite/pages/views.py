df2bce47317d7b86a8e34b75e888b949117645ba
Javier <ing.jmpinillag@gmail.com>
2020-12-12 23:09:19 +0100

    adding context to the http response

--- src/githubSite/pages/views.py
+++ src/githubSite/pages/views.py
@@ -4,8 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "home.html", {})
 
 def contact_view(request, *args, **kwargs):
-    print(request.user)
-    return HttpResponse("<h1>Hello contact</h1>")    
+    my_context = {
+        "my_text": "this is about us",
+        "my_number": 14        
+    }
+    return render(request, "contact.html", my_context)    
